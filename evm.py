c5bb5c4235863185aa7771d76143a57ad4136ba3
Andrey Sobol <andreysobol@protonmail.com>
2023-04-15 14:40:52 +0100

    metaopcode_math

--- evm.py
+++ evm.py
@@ -5,3 +5,20 @@ class EVMState:
         self.memory = bytearray()
         self.storage = {}
         self.pc = 0
+
+def metaopcode_math(state, op, size) -> EVMState:
+
+    if size == 1:
+        x = state.stack.pop()
+        state.stack = state.stack + op(x)
+    elif size == 2:
+        x = state.stack.pop()
+        y = state.stack.pop()
+        state.stack = state.stack + op(x, y)
+    elif size == 3:
+        x = state.stack.pop()
+        y = state.stack.pop()
+        z = state.stack.pop()
+        state.stack = state.stack + op(x, y, z)
+
+    return state
